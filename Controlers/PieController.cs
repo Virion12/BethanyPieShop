a8cc7650666fe8cf3703d70c30bf53b14ab60398
Virion12 <dgiwebapps@gmail.com>
2025-07-31 20:36:55 +0200

    First commit

---
+++ Controlers/PieController.cs
@@ -0,0 +1,28 @@
+﻿using BethanyPieShop.Models;
+using BethanyPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShop.Controlers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _PieRepository;
+        private readonly ICategoryRepository _CategoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+            {
+                _PieRepository = pieRepository;
+                _CategoryRepository = categoryRepository;
+        }
+
+        public IActionResult List()
+        {
+            //ViewBag.CurrentCategory = "Chees Cake";
+            //return View(_PieRepository.AllPies);
+            PieListViewModel pieListViewModel = new PieListViewModel(_PieRepository.AllPies, "Chees cakes");
+            return View(pieListViewModel);
+        }
+
+
+    }
+}
---
+++ Models/MockCategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace BethanyPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories => 
+        new List<Category> 
+        {
+            new Category{CategoryId=1, CategoryName = "Fruit pies", Description = "All-fruit pies"},
+            new Category{CategoryId=2, CategoryName = "Chees cakes", Description = "Cheesy all the way"},
+            new Category{CategoryId=3, CategoryName = "Sesonal pies", Description = "Sesonal pies"},
+        };
+    }
+}
