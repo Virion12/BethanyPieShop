c2df8c7a88853dee5e75c3c2611afd0cedabc3ad
Virion12 <dgiwebapps@gmail.com>
2025-08-04 20:39:50 +0200

    Imporved views and created partail view for Category navigation

---
+++ Controlers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShop.Controlers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Controlers/PieController.cs
+++ Controlers/PieController.cs
@@ -1,41 +1,58 @@
 ﻿using BethanyPieShop.Models;
 using BethanyPieShop.ViewModels;
+using BethanysPieShop.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BethanyPieShop.Controlers
+namespace BethanysPieShop.Controllers
 {
     public class PieController : Controller
     {
-        private readonly IPieRepository _PieRepository;
-        private readonly ICategoryRepository _CategoryRepository;
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
 
         public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
-            {
-                _PieRepository = pieRepository;
-                _CategoryRepository = categoryRepository;
-        }
-
-        public IActionResult List()
         {
-            //ViewBag.CurrentCategory = "Chees Cake";
-            //return View(_PieRepository.AllPies);
-            PieListViewModel pieListViewModel = new PieListViewModel(_PieRepository.AllPies, "All pies");
-            return View(pieListViewModel);
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
         }
 
+        //public IActionResult List()
+        //{
+        //    //ViewBag.CurrentCategory = "Cheese cakes";
+
+        //    //return View(_pieRepository.AllPies);
+
+        //    PieListViewModel piesListViewModel = new PieListViewModel(_pieRepository.AllPies, "Cheese cakes");
+        //    return View(piesListViewModel);
+        //}
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string? currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PieListViewModel(pies, currentCategory));
+        }
 
         public IActionResult Details(int id)
         {
-            var pie = _PieRepository.GetPieById(id);
+            var pie = _pieRepository.GetPieById(id);
             if (pie == null)
-            {
                 return NotFound();
-            }
+
             return View(pie);
-
         }
-
-
-
     }
 }
